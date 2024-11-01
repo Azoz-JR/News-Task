38e076d46e1a2f30ae685c8847f1cbf8c2a7f22f
Abdelaziz Salah <azozsalah19@gmail.com>
2024-11-01 22:22:23 +0200

    Initial Commit

---
+++ NewsTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsTask
+//
+//  Created by Azoz Salah on 01/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
